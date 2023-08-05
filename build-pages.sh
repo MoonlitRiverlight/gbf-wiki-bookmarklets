e538e2c23aed9739f0f6af07230357145cddec5f
MoonlitRiverlight <100371672+MoonlitRiverlight@users.noreply.github.com>
2023-08-05 14:28:54 -0300

    Update build-pages.sh

--- build-pages.sh
+++ build-pages.sh
@@ -5,6 +5,7 @@ cp index.html public/index.html
 sed -i "s/#raid/$(cat dist/raid.js)/" public/index.html
 sed -i "s/#character/$(cat dist/character.js)/" public/index.html
 sed -i "s/#weapon/$(cat dist/weapon.js)/" public/index.html
-sed -i "s/#weapon_JP/$(cat dist/weapon.js)/" public/index.html
+sed -i "s/#jpweapon/$(cat dist/jpweapon.js)/" public/index.html
 sed -i "s/#summon/$(cat dist/summon.js)/" public/index.html
+sed -i "s/#jpsummon/$(cat dist/jpsummon.js)/" public/index.html
 sed -i "s/#date/$(date)/" public/index.html
