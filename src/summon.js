290d60a42bd529f47548c7d4f717ab2070da663d
MoonlitRiverlight <100371672+MoonlitRiverlight@users.noreply.github.com>
2023-08-05 14:08:58 -0300

    Update summon.js
    Incorporate Turtleli's fixes, update fields.

--- src/summon.js
+++ src/summon.js
@@ -8,12 +8,19 @@ if (!location.hash.match("#gacha/summon")) {
 
 const ID = document.querySelector('.prt-summon-image > .img-summon').getAttribute("src").match(/\/b\/(.+?)\./)[1];
 
-const NAME = document.querySelector(".prt-summon-info > div:first-child").textContent;
+const NAME = (() => {
+  const largeBanner = document.querySelector(".txt-item-name")?.textContent;
+  const smallBanner = document.querySelector(".prt-summon-info > div:first-child")?.textContent;
+  return (largeBanner) ? largeBanner : smallBanner;
+})();
 
 const RARITY = (() => {
   if (document.querySelector(".prt-rarity-4")) return "ssr";
   if (document.querySelector(".prt-rarity-3")) return "sr";
   if (document.querySelector(".prt-rarity-2")) return "r";
+  if (document.querySelector(".prt-rarity-4-large")) return "ssr";
+  if (document.querySelector(".prt-rarity-3-large")) return "sr";
+  if (document.querySelector(".prt-rarity-2-large")) return "r";
   return "?";
 })();
 
@@ -34,62 +41,86 @@ const MAX_HP = document.querySelector(".prt-max-hp .txt-hp-value").textContent;
 const MIN_ATK = document.querySelector(".prt-min-atk .txt-atk-value").textContent;
 const MAX_ATK = document.querySelector(".prt-max-atk .txt-atk-value").textContent;
 
-const CALL_NAME = document.querySelector(".prt-detail-special .name").textContent;
-const CALL_DESC = document.querySelector(".prt-detail-special .comment").textContent;
+const call_info = document.querySelector("#prt-detail-summon-special-skill");
+const CALL_NAME = call_info?.querySelector(".name")?.textContent;
+const CALL_DESC = call_info?.querySelector(".comment")?.textContent;
+const CALL_COOLDOWN = call_info?.querySelector(".txt-recast")?.textContent.match(/\d+/)?.[0];
+const CALL_COOLDOWN_FIRST = call_info?.querySelector(".txt-start-recast")?.textContent.match(/\d+/)?.[0];
 
-const AURA_NAME = document.querySelector(".prt-detail-support .name").textContent;
-const MAIN_AURA_DESC = document.querySelector(".prt-detail-support .prt-aura-description").textContent.trim();
-const SUB_AURA_DESC = document.querySelector(".prt-detail-support .prt-aura-description ~ .prt-aura-description")?.textContent.trim();
+const AURA_NAME = document.querySelector(".prt-detail-protection .name")?.textContent;
+const MAIN_AURA_DESC = document.querySelector(".prt-detail-protection .prt-aura-description")?.textContent.trim();
+const SUB_AURA_DESC = document.querySelector(".prt-detail-protection .prt-aura-description ~ .prt-aura-description")?.textContent.trim();
 
 const FLAVOR = document.querySelector(".prt-flavor").textContent.trim();
 
 const result = nonEmpty`{{Summon
-|id= ${ID}
+|id=${ID}
 |summonid=
-|element= ${ELEMENT}
-|rarity= ${RARITY}
+|element=${ELEMENT}
+|rarity=${RARITY}
 |homescreen=
-|name= ${NAME}
+|series=
+|name=${NAME}
 |jpname=
-|link_jpwiki=
-|link_gamewith=
-|link_kamigame=
-|va= ${VOICE_ACTOR}
+|va=${VOICE_ACTOR}
 |vajp=
-|base_evo= 3
-|max_evo= 3
-|image={{PAGENAME}}
-|hp1= ${MIN_HP}
-|hp2= ${MAX_HP}
-|hp3=
-|atk1= ${MIN_ATK}
-|atk2= ${MAX_ATK}
-|atk3=
-|release_date= 
+|release_date=
 |4star_date=
 |4star_unlock=
-|obtain= 
-|join= 
-|aura_name= ${AURA_NAME}
-|aura1= ${MAIN_AURA_DESC}
+|5star_date=
+|5star_unlock=
+|obtain=
+|join=
+
+|min_evo=0
+|base_evo=3
+|max_evo=3
+
+|image={{PAGENAME}}
+
+|link_jpwiki=
+|link_gamewith=
+
+|hp1=${MIN_HP}
+|hp2=${MAX_HP}
+|hp3=
+
+|atk1=${MIN_ATK}
+|atk2=${MAX_ATK}
+|atk3=
+
+|aura_name=${AURA_NAME}
+|enaura1=${MAIN_AURA_DESC}
+|enaura2=
+|enaura3=
+|aura1=${MAIN_AURA_DESC}
 |aura2=
 |aura3=
-|subaura1= ${SUB_AURA_DESC}
+
+|ensubaura1=${SUB_AURA_DESC}
+|ensubaura2=
+|subaura1=${SUB_AURA_DESC}
 |subaura2=
-|call_name= ${CALL_NAME}
-|call_cd_first=
+
+|call_name=${CALL_NAME}
+|call_cd_first=${CALL_COOLDOWN_FIRST}
 |call_cd_first_mlb=
 |call_cd_first_flb=
-|call_cd=
+|call_cd=${CALL_COOLDOWN}
 |call_cd_mlb=
 |call_cd_flb=
 |call_reuse=
 |comboable=
 |combo1=
 |combo2=
-|call_base= ${CALL_DESC}
+
+|encall_base=${CALL_DESC}
+|encall_mlb=
+|encall_flb=
+|call_base=${CALL_DESC}
 |call_mlb=
 |call_flb=
+
 |flb1=
 |flb2=
 |flb3=
@@ -99,9 +130,11 @@ const result = nonEmpty`{{Summon
 |flb7=
 |flb8=
 |rupies=
-|flavor= ${FLAVOR}
+
 |reduce=
 |reduce_advice=
+
+|flavor=${FLAVOR}
 }}
 
 ==Gameplay Notes==
