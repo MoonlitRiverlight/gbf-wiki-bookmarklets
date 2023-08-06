e2e6d61428146b62dd0a10478e2102b9be76e59f
MoonlitRiverlight <100371672+MoonlitRiverlight@users.noreply.github.com>
2023-08-06 16:22:02 -0300

    Update jpweapon.js

--- src/jpweapon.js
+++ src/jpweapon.js
@@ -15,14 +15,43 @@ const NAME = (() => {
 })();
 
 const RARITY = (() => {
-  if (document.querySelector(".prt-rarity-4")) return "SSR";
-  if (document.querySelector(".prt-rarity-3")) return "SR";
-  if (document.querySelector(".prt-rarity-2")) return "R";
-  if (document.querySelector(".prt-rarity-4-large")) return "SSR";
-  if (document.querySelector(".prt-rarity-3-large")) return "SR";
-  if (document.querySelector(".prt-rarity-2-large")) return "R";
+  if (document.querySelector(".prt-rarity-4")) return "ssr";
+  if (document.querySelector(".prt-rarity-3")) return "sr";
+  if (document.querySelector(".prt-rarity-2")) return "r";
+  if (document.querySelector(".prt-rarity-4-large")) return "ssr";
+  if (document.querySelector(".prt-rarity-3-large")) return "sr";
+  if (document.querySelector(".prt-rarity-2-large")) return "r";
   return "?";
 })();
+
+const ELEMENT = (() => {
+  if (document.querySelector(".ico-type1")) return "fire";
+  if (document.querySelector(".ico-type2")) return "water";
+  if (document.querySelector(".ico-type3")) return "earth";
+  if (document.querySelector(".ico-type4")) return "wind";
+  if (document.querySelector(".ico-type5")) return "light";
+  if (document.querySelector(".ico-type6")) return "dark";
+  return "?";
+})();
+
+const TYPE = (() => {
+  if (document.querySelector(".ico-weapon-1")) return "sabre";
+  if (document.querySelector(".ico-weapon-2")) return "dagger";
+  if (document.querySelector(".ico-weapon-3")) return "spear";
+  if (document.querySelector(".ico-weapon-4")) return "axe";
+  if (document.querySelector(".ico-weapon-5")) return "staff";
+  if (document.querySelector(".ico-weapon-6")) return "gun";
+  if (document.querySelector(".ico-weapon-7")) return "melee";
+  if (document.querySelector(".ico-weapon-8")) return "bow";
+  if (document.querySelector(".ico-weapon-9")) return "harp";
+  if (document.querySelector(".ico-weapon-10")) return "katana";
+  return "?";
+})();
+
+const MIN_HP = document.querySelector(".prt-min-hp .txt-hp-value").textContent;
+const MAX_HP = document.querySelector(".prt-max-hp .txt-hp-value").textContent;
+const MIN_ATK = document.querySelector(".prt-min-atk .txt-atk-value").textContent;
+const MAX_ATK = document.querySelector(".prt-max-atk .txt-atk-value").textContent;
 
 const OUGI_NAME = document.querySelector(".prt-detail-special .name-m").textContent;
 const OUGI_DESC = document.querySelector(".prt-detail-special .comment-m").textContent;
@@ -34,27 +63,18 @@ const SKILL_ONE_DESC = document.querySelector(".prt-detail-support > .prt-box:nt
 const SKILL_TWO_NAME = document.querySelector(".prt-detail-support > .prt-box:nth-child(3) > .name-m")?.textContent;
 const SKILL_TWO_ICON = document.querySelector(".prt-detail-support > .prt-box:nth-child(3) > :first-child")?.className.replace("ico-", "ws_")+".png";
 const SKILL_TWO_DESC = document.querySelector(".prt-detail-support > .prt-box:nth-child(3) > .comment-m")?.textContent;
+const SKILL_TWO_LEVELREQ = document.querySelector(".prt-detail-support > .not-get:nth-child(3) .txt-condition-level")?.textContent.match(/\d+$/)?.[0];
 
 const SKILL_THREE_NAME = document.querySelector(".prt-detail-support > .prt-box:nth-child(4) > .name-m")?.textContent;
 const SKILL_THREE_DESC = document.querySelector(".prt-detail-support > .prt-box:nth-child(4) > .comment-m")?.textContent;
 const SKILL_THREE_ICON = document.querySelector(".prt-detail-support > .prt-box:nth-child(4) > :first-child")?.className.replace("ico-", "ws_")+".png";
+const SKILL_THREE_LEVELREQ = document.querySelector(".prt-detail-support > .not-get:nth-child(4) .txt-condition-level")?.textContent.match(/\d+$/)?.[0];
 
 const FLAVOR = document.querySelector(".prt-flavor").textContent.trim();
-
-const result = nonEmpty`{{Weapon
-|id=${ID}
-|series=
+const result = nonEmpty`
 |jpname=${NAME}
 
 |link_jpwiki=武器/${NAME}+%28${RARITY}%29
-
-|hp1=${MIN_HP}
-|hp2=${MAX_HP}
-|hp3=
-
-|atk1=${MIN_ATK}
-|atk2=${MAX_ATK}
-|atk3=
 
 |jpougi_name=${OUGI_NAME}
 |jpougi=${OUGI_DESC}
@@ -76,7 +96,6 @@ const result = nonEmpty`{{Weapon
 |jps3_4s_desc=
 
 |jpflavor=${FLAVOR}
-}}
 `;
 
 copyToClipboard(result);
